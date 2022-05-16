62c6cf4b5cae83e2b7c825c554ff172c55eb6023
DeLosReyesLyshane <lyshanedelosreyes@gmail.com>
2022-05-17 04:00:01 +0800

    Singnoto Added

--- index.php
+++ index.php
@@ -247,28 +247,26 @@ foreach($file->profile as $rows) {
 
         <div class="section-title">
           <h2>Portfolio</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
         </div>
 
         <div class="row" data-aos="fade-up">
           <div class="col-lg-12 d-flex justify-content-center">
             <ul id="portfolio-flters">
               <li data-filter="*" class="filter-active">All</li>
-              <li data-filter=".filter-app">App</li>
-              <li data-filter=".filter-card">Card</li>
-              <li data-filter=".filter-web">Web</li>
+              <li data-filter=".filter-projects">Projects</li>
+              <li data-filter=".filter-ads">Advertisement</li>
             </ul>
           </div>
         </div>
 
         <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="100">
 
-          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+          <div class="col-lg-4 col-md-6 portfolio-item filter-projects">
             <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
+              <img src="assets/img/portfolio/Singnoto.png" class="img-fluid" alt="">
               <div class="portfolio-links">
-                <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 1"><i class="bx bx-plus"></i></a>
-                <a href="portfolio-details.php" title="More Details"><i class="bx bx-link"></i></a>
+                <a href="assets/img/portfolio/Singnoto.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Singnoto"><i class="bx bx-plus"></i></a>
+                <a href="Singnoto.php" title="More Details"><i class="bx bx-link"></i></a>
               </div>
             </div>
           </div>
